d3c4752a262b6c1e6f70c8283b92546e9c28faa4
Nafiul-Islam-Eshan <nafiulislameshan307@gmail.com>
2026-08-21 22:51:00 +0600

    Next js setup, navbar is now cooking...

---
+++ src/Component/navbar/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NavbarPage = () => {
+    return (
+        <div>
+            <h1 className="text-3xl text-blue-400">It{`'`}s navbar</h1>
+        </div>
+    );
+};
+
+export default NavbarPage;
