98fd0cc6a3d6b94edb47cb379195c4b9d42bdd69
Nafiul-Islam-Eshan <nafiulislameshan307@gmail.com>
2026-08-22 12:09:15 +0600

    Started footer designing

---
+++ src/Component/Footer/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FooterPage = () => {
+    return (
+        <footer className='bg-[#244D3F] text-4xl font-serif text-violet-700 font-extrabold' >
+            It is footer page
+        </footer>
+    );
+};
+
+export default FooterPage;
