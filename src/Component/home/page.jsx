8804a6d90127e016bd17d89004c59cbf710acb63
Nafiul-Islam-Eshan <nafiulislameshan307@gmail.com>
2026-08-22 20:26:50 +0600

    HomePage: Banner designing done, summery cards desgining done

---
+++ src/Component/home/page.jsx
@@ -0,0 +1,67 @@
+
+
+const HomePage = () => {
+  return (
+    <div>
+      <div className="flex flex-col p-5 gap-5">
+        <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold text-center">
+          Friends to keep close in your life
+        </h1>
+
+        <p className="text-md opacity-70 py-2 md:px-60 text-center">
+          Your personal shelf of meaningful connections. Browse, tend, and
+          nurture the relationships that matter most.
+        </p>
+
+        <button className="w-[148] mx-auto btn bg-[#244D3F] text-white">
+          + Add a Friend
+        </button>
+      </div>
+
+      {/* Summary cards */}
+      <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 my-10">
+        {/* Card 1 = Total Friend */}
+        <div className="card h-[137] w-[265] md:w-full mx-auto bg-base-100 shadow-md">
+          <div className="card-body justify-center items-center">
+            <div className="flex flex-col text-center gap-2">
+              <h2 className="text-3xl font-bold text-[#244D3F]">10</h2>
+              <p className="text-md opacity-70">Total Friends</p>
+            </div>
+          </div>
+        </div>
+
+        {/* Card 2 = On track */}
+        <div className="card h-[137] w-[265] md:w-full mx-auto bg-base-100 shadow-md">
+          <div className="card-body justify-center items-center">
+            <div className="flex flex-col text-center gap-2">
+              <h2 className="text-3xl font-bold text-[#244D3F]">3</h2>
+              <p className="text-md opacity-70">On Trace</p>
+            </div>
+          </div>
+        </div>
+
+        {/* Card 3 = Need attention*/}
+        <div className="card h-[137] w-[265] md:w-full mx-auto bg-base-100 shadow-md">
+          <div className="card-body justify-center items-center">
+            <div className="flex flex-col text-center gap-2">
+              <h2 className="text-3xl font-bold text-[#244D3F]">6</h2>
+              <p className="text-md opacity-70">Need Attention</p>
+            </div>
+          </div>
+        </div>
+
+        {/* Card 4 = Interactions This month */}
+        <div className="card h-[137] w-[265] md:w-full mx-auto bg-base-100 shadow-md">
+          <div className="card-body justify-center items-center">
+            <div className="flex flex-col text-center gap-2">
+              <h2 className="text-3xl font-bold text-[#244D3F]">12</h2>
+              <p className="text-md opacity-70">Interactions This Month</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,9 @@
+import HomePage from "@/Component/home/page";
+
 export default function Home() {
   return (
     <main className="flex flex-col flex-1 items-center justify-center lg:px-[245] md:px-[150]">
-
-      <div>
-        <h1 className="text-3xl font-bold text-center">Friends to keep close in your life</h1>
-      </div>      
+      <HomePage />
     </main>
   );
 }
